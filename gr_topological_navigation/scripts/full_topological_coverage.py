ca506482d06dccffe2c39cc6324c22e593741413
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-02-26 16:50:13 +0100

    error in script for exit conditions solved

--- gr_topological_navigation/scripts/full_topological_coverage.py
+++ gr_topological_navigation/scripts/full_topological_coverage.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     topological_planner.generate_full_coverage_plan()
     topological_planner.go_to_source()
 
-    next_transition = True
+    next_transition = topological_planner.get_next_transition()
 
     while (next_transition):
-        next_transition = topological_planner.get_next_transition()
         topological_planner.command_robot_to_node(next_transition[1])
         topological_planner.show_statistics()
+        next_transition = topological_planner.get_next_transition()
