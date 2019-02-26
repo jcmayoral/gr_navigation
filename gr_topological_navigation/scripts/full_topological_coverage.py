90c56e957e575bfcf63b6149617be90d42eaa4f9
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-02-26 16:18:28 +0100

    add start_node option version 0.0.3 gr_topological_navigation

--- gr_topological_navigation/scripts/full_topological_coverage.py
+++ gr_topological_navigation/scripts/full_topological_coverage.py
@@ -5,8 +5,11 @@ from gr_topological_navigation.topological_planner import TopologicalPlanner
 if __name__ == '__main__':
     topological_planner = TopologicalPlanner()
     topological_planner.generate_full_coverage_plan()
+    topological_planner.go_to_source()
 
-    next_transition = topological_planner.get_next_transition()
+    next_transition = True
+
     while (next_transition):
-        topological_planner.command_robot_to_node(next_transition[1])
         next_transition = topological_planner.get_next_transition()
+        topological_planner.command_robot_to_node(next_transition[1])
+        topological_planner.show_statistics()
--- gr_topological_navigation/src/gr_topological_navigation/topological_planner.py
+++ gr_topological_navigation/src/gr_topological_navigation/topological_planner.py
@@ -7,12 +7,14 @@ from strands_navigation_msgs.srv import GetTopologicalMapRequest, GetTopological
 from topological_navigation.msg import GotoNodeAction, GotoNodeGoal
 
 class TopologicalPlanner:
-    def __init__(self, gui=False):
+    def __init__(self, gui=False, start_node='cold_storage'):
         rospy.init_node("topological_full_coverage_planner")
         get_topological_map = rospy.ServiceProxy("/topological_map_publisher/get_topological_map", GetTopologicalMap)
         msg = GetTopologicalMapRequest()
         #TODO ask for the parameter
         msg.pointset = "simulation_map"
+        #TODO ask for the parameter
+        self.start_node = start_node
         #TODO what to do in case of failures
         map = get_topological_map(msg)
         #Topological Map parsed to a networkx Graph so functions can be used
@@ -39,8 +41,13 @@ class TopologicalPlanner:
             plt.show()
 
     #TODO ask for current edge or node
+    #Add distance weight to the edges
     def generate_full_coverage_plan(self):
-        self.topological_plan = list(nx.edge_dfs(self.networkx_graph))
+        self.topological_plan = list(nx.edge_dfs(self.networkx_graph, source=self.start_node))
+
+    def go_to_source(self):
+        rospy.loginfo("Robot going to start node %s", self.start_node)
+        self.command_robot_to_node(self.start_node, no_orientation=False)
 
     def get_next_transition(self):
         if self.topological_plan is None:
@@ -51,12 +58,15 @@ class TopologicalPlanner:
         next_node = self.topological_plan.pop(0)
         return next_node
 
-    def command_robot_to_node(self,node_id):
+    def command_robot_to_node(self,node_id, no_orientation=True):
         rospy.loginfo("Commanding robot to %s", node_id)
         navgoal = GotoNodeGoal()
         navgoal.target = node_id
-        navgoal.no_orientation = True
+        navgoal.no_orientation = no_orientation
         self.action_client.send_goal(navgoal)
         self.action_client.wait_for_result()
-        rospy.logwarn("%d nodes still missing", len(self.topological_plan))
-        rospy.loginfo("Is node %s reached? %r", node_id,  self.action_client.get_result())  # A FibonacciResul
+        rospy.loginfo("Is node %s reached? %r", node_id,  self.action_client.get_result())
+
+    #TODO add more statistics
+    def show_statistics(self):
+        rospy.logwarn("%d edges still missing", len(self.topological_plan))
