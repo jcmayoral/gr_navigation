a7fb0577b7b89a9c9372b22572c017c0bec45b76
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-03-05 17:27:43 +0100

    gr_pointcloud_filter minimal compiling

--- gr_pointcloud_filter/include/gr_pointcloud_filter/pointcloud_filter.h
+++ gr_pointcloud_filter/include/gr_pointcloud_filter/pointcloud_filter.h
@@ -1,12 +1,26 @@
 #include <nodelet/nodelet.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
 
 namespace gr_pointcloud_filter
 {
 
     class MyNodeletClass : public nodelet::Nodelet
     {
-        public:
+    	private:
+    		ros::Subscriber pointcloud_sub_;
+    		ros::Publisher pointcloud_pub_;
+    	public:
             virtual void onInit();
+            void pointcloud_cb(sensor_msgs::PointCloud2ConstPtr msg);
+            void applyFilters(sensor_msgs::PointCloud2 msg);
+
     };
 
 }
--- gr_pointcloud_filter/src/pointcloud_filter.cpp
+++ gr_pointcloud_filter/src/pointcloud_filter.cpp
@@ -9,11 +9,98 @@ PLUGINLIB_EXPORT_CLASS(gr_pointcloud_filter::MyNodeletClass, nodelet::Nodelet)
 
 namespace gr_pointcloud_filter
 {
+	/*
+	MyNodeletClass::MyNodeletClass(){
+		ROS_INFO("My NodeletClass constructor");
+		ros::NodeHandle nh;
+		nh.subscribe("velodyne_points", 10, &MyNodeletClass::pointcloud_cb, this);
+		ROS_INFO("Nodelet has been initialized");
+	}
+	*/
+
+    void MyNodeletClass::applyFilters(sensor_msgs::PointCloud2 msg){
+    	ROS_INFO("applying filters");
+
+    	/*
+    	pcl::PCLPointCloud2 pcl_pc2;// = new pcl::PCLPointCloud2;
+    	//pcl::PCLPointCloud2 cloud_filtered;
+    	// Convert to PCL data type
+    	pcl_conversions::toPCL(msg, pcl_pc2);
+    	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    	pcl::fromPCLPointCloud2(pcl_pc2,*cloud);
+    	 */
+
+
+    	// Convert the sensor_msgs/PointCloud2(cloud2) object to pcl/pointcloud(cloud1) object with fromROSMsg()
+    	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+    	pcl::fromROSMsg (msg, *cloud);
+
+
+
+    	// Downsample not used (Velodyne is already sparsed
+    	/*
+    	pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    	sor.setInputCloud (cloudPtr);
+    	sor.setLeafSize (0.1, 0.1, 0.1);
+    	sor.filter (cloud_filtered);
+		*/
+
+    	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+    	pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+    	// Create the segmentation object
+    	pcl::SACSegmentation<pcl::PointXYZ> seg;
+    	// Optional
+    	seg.setOptimizeCoefficients (true);
+    	// Mandatory
+    	seg.setModelType (pcl::SACMODEL_PLANE);
+    	seg.setMethodType (pcl::SAC_RANSAC);
+    	seg.setMaxIterations (1000);
+    	seg.setDistanceThreshold (0.01);
+		//seg.setInputCloud (cloud);
+   	    seg.segment (*inliers, *coefficients);
+
+
+   	    if (inliers->indices.size () == 0)
+   	    {
+   	    	return;
+		}
+
+    	// Create the filtering object
+    	pcl::ExtractIndices<pcl::PointXYZ> extract;
+		// Extract the inliers
+   	    extract.setInputCloud (cloud);
+		extract.setIndices (inliers);
+		extract.setNegative (false);
+		//extract.filter(cloud);
+
+
+
+    	// Convert to ROS data type
+    	sensor_msgs::PointCloud2 output;
+    	//pcl_conversions::fromPCL(cloud, output);
+
+
+    	//Convert pcl/pointcloud object(cloud1) to sensor_msgs/PointCloud2(cloud2) with toROSMsg()
+    	pcl::toROSMsg(*cloud, output);
+    	// Publish the data
+    	pointcloud_pub_.publish (output);
+    }
+
+
+    void MyNodeletClass::pointcloud_cb(sensor_msgs::PointCloud2ConstPtr msg){
+    	ROS_DEBUG("Callback working");
+    	applyFilters(*msg);
+    }
+
     void MyNodeletClass::onInit()
     {
 
-      for (auto i=0; i<100; ++i)
-        std::cout << i << std::endl;
-      NODELET_DEBUG("Initializing nodelet...");
+    	ROS_INFO("My NodeletClass constructor");
+		ros::NodeHandle nh;
+		pointcloud_sub_ = nh.subscribe("/velodyne_points", 10, &MyNodeletClass::pointcloud_cb, this);
+		pointcloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/velodyne_points/filtered", 10);
+		NODELET_DEBUG("Initializing nodelet...");
+		NODELET_INFO("Here");
+		//ros::spin();
     }
 }
