289c4b42932b49eb1a2d4c480bb7327a6b45f69b
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-03-08 16:06:14 +0100

    gr_map_utils modifications

--- gr_map_utils/include/gr_map_utils/map_converter_interface.h
+++
@@ -1,8 +0,0 @@
-namespace gr_map_utils{
-    class MapConverterInterface{
-        public:
-            virtual bool storeMap() = 0;
-            virtual bool getMap() = 0;
-            virtual ~MapConverterInterface(){}
-    };
-};
