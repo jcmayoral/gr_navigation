8abfb66e27809292cd1ef1c6925b78fe64cbc712
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-03-13 15:02:43 +0100

    gr_map_utils 1.0.5

--- gr_map_utils/include/gr_map_utils/osm_to_topological_converter.h
+++ gr_map_utils/include/gr_map_utils/osm_to_topological_converter.h
@@ -22,8 +22,10 @@ namespace gr_map_utils{
             void osm_map_cb(const visualization_msgs::MarkerArray::ConstPtr& map);
         private:
             visualization_msgs::MarkerArray osm_map_;
+            visualization_msgs::MarkerArray filtered_map_;
             strands_navigation_msgs::TopologicalMap topological_map_;
             ros::Publisher topological_map_pub_;
+            ros::Publisher topological_marker_pub_;
             ros::Subscriber osm_map_sub_;
             ros::NodeHandle nh_;
             std::mutex mutex_;
--- gr_map_utils/src/osm_to_topological_converter.cpp
+++ gr_map_utils/src/osm_to_topological_converter.cpp
@@ -2,10 +2,12 @@
 
 namespace gr_map_utils{
 
-    Osm2TopologicalMap::Osm2TopologicalMap(ros::NodeHandle nh): nh_(nh){
+    Osm2TopologicalMap::Osm2TopologicalMap(ros::NodeHandle nh): nh_(nh), osm_map_(){
         gr_tf_publisher_ = new TfFramePublisher();
         message_store_ = new mongodb_store::MessageStoreProxy(nh);
-        topological_map_pub_ = nh_.advertise<strands_navigation_msgs::TopologicalMap>("topological_map", 1, true); 
+        topological_map_pub_ = nh_.advertise<strands_navigation_msgs::TopologicalMap>("topological_map", 1, true);
+        topological_marker_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("topological_map_2", 1, true); 
+
         osm_map_sub_ = nh_.subscribe("visualization_marker_array",10, &Osm2TopologicalMap::osm_map_cb, this);
     }
 
@@ -35,11 +37,19 @@ namespace gr_map_utils{
     }
 
     void Osm2TopologicalMap::transformMap(){
-        ROS_INFO("TODO");
+        std::unique_lock<std::mutex> lk(mutex_);
         int count = 0;
+        strands_navigation_msgs::TopologicalNode node;
+        filtered_map_.markers.clear();
+        topological_map_.nodes.clear();
+
         for (std::vector<visualization_msgs::Marker>::iterator it = osm_map_.markers.begin(); it != osm_map_.markers.end(); ++it){
+             ROS_INFO("TODO");
             if (it->type == visualization_msgs::Marker::CYLINDER){
                 count ++;
+                node.pose = it->pose;
+                filtered_map_.markers.emplace_back(*it);
+                topological_map_.nodes.emplace_back(node);
             }
         }
         std::cout<< count ;
@@ -50,6 +60,9 @@ namespace gr_map_utils{
     }
 
     void Osm2TopologicalMap::publishMaps(){
+        std::cout << "NODES " << topological_map_.nodes.size() << std::endl;
+        topological_marker_pub_.publish(filtered_map_);
+        topological_map_pub_.publish(topological_map_);
     }
 
 }
