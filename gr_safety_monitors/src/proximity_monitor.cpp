1f09aea138feb419b182333a5b05f5097e9f6811
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-03-21 14:11:38 +0100

    gr_safety_monitors 0.0.6

--- gr_safety_monitors/src/proximity_monitor.cpp
+++ gr_safety_monitors/src/proximity_monitor.cpp
@@ -8,17 +8,36 @@ PLUGINLIB_DECLARE_CLASS(gr_safety_monitors, ProximityMonitor,
                         fault_core::FaultDetector)
 
 using namespace fault_core;
-using namespace message_filters;
+//using namespace message_filters;
 namespace gr_safety_monitors
 {
 
   void ProximityMonitor::instantiateServices(ros::NodeHandle nh){
     marker_pub_ = nh.advertise<visualization_msgs::MarkerArray>("proximity_visualization", 1);
-
-
+    pointcloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>("proximity_pointcloud", 1);
+    pointcloud_sub_ = nh.subscribe("/velodyne_points/filtered", 2, &ProximityMonitor::pointcloud_CB, this);
   }
 
-  ProximityMonitor::ProximityMonitor(): is_obstacle_detected_(false), region_radius_(0.5), regions_number_(3)
+  void ProximityMonitor::pointcloud_CB(const sensor_msgs::PointCloud2::ConstPtr& pointcloud){
+    //pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    //pcl::fromROSMsg(pointcloud, *cloud);
+    //pcl::fromPCLPointCloud2(*pointcloud, *cloud);
+    pcl::PCLPointCloud2 pcl_pc;
+	  pcl_conversions::toPCL(*pointcloud, pcl_pc);
+  	pcl::PointCloud<pcl::PointXYZRGB> cloud;
+	  pcl::fromPCLPointCloud2(pcl_pc, cloud);
+	  //pcl2 to pclxyzrgba
+	  //pcl::PointCloud<PointXYZRGBA> input_cloud;
+	  //pcl::fromPCLPointCloud2(pcl_pc, input_cloud);
+
+    // Convert to ROS data type
+    sensor_msgs::PointCloud2 output_pointcloud;
+    pcl::toROSMsg(cloud, output_pointcloud);
+    // Publish the data
+    pointcloud_pub_.publish(output_pointcloud);
+  }
+
+  ProximityMonitor::ProximityMonitor(): is_obstacle_detected_(false), region_radius_(25), regions_number_(3)
   {
     //Define Fault Type as Unknown
     fault_.type_ =  FaultTopology::UNKNOWN_TYPE;
@@ -45,7 +64,7 @@ namespace gr_safety_monitors
   }
 
   void ProximityMonitor::updateMarker(visualization_msgs::Marker& marker, int level){
-    marker.header.frame_id = "base_link";
+    marker.header.frame_id = "velodyne";
     marker.header.stamp = ros::Time::now();
     marker.ns = "proximity_regions"+std::to_string(level);
     marker.type = visualization_msgs::Marker::LINE_STRIP;
@@ -63,15 +82,14 @@ namespace gr_safety_monitors
 
     geometry_msgs::Point circumference;
     float r_2 = pow(region_radius_*level,2);
-    marker.points.clear();
 
-    for (float y= -region_radius_*level; y<=region_radius_*level; y+=0.025 ){
+    for (float y= -region_radius_*level; y<=region_radius_*level; y+=0.5 ){
           circumference.x = sqrt(r_2 - pow(y,2));
           circumference.y = y;
           marker.points.push_back(circumference);
     }
     //mirror
-    for (float y= region_radius_*level; y>=-region_radius_*level; y-=0.025 ){
+    for (float y= region_radius_*level; y>=-region_radius_*level; y-=0.5 ){
           circumference.x = -sqrt(r_2 - pow(y,2));
           circumference.y = y;
           marker.points.push_back(circumference);
