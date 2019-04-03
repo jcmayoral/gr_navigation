04d44329e72e90d8e7d35c22a163447026805916
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-04-03 13:59:36 +0200

    gr_safety_monitors 0.1.3

--- gr_safety_monitors/src/safe_actions/dynamic_reconfigure_safe_action.cpp
+++ gr_safety_monitors/src/safe_actions/dynamic_reconfigure_safe_action.cpp
@@ -14,13 +14,25 @@ void DynamicReconfigureSafeAction::execute(){
     dynamic_reconfigure::ReconfigureResponse srv_resp;
     dynamic_reconfigure::DoubleParameter double_param;
     dynamic_reconfigure::Config conf;
-    
+
     double_param.name = "max_trans_vel";
-    double_param.value = 20;
+    double_param.value = 0.25;
     conf.doubles.push_back(double_param);
 
     double_param.name = "min_trans_vel";
     double_param.value = 0;
+    conf.doubles.push_back(double_param);
+
+    double_param.name = "max_vel_x";
+    double_param.value = 0.2;
+    conf.doubles.push_back(double_param);
+
+    double_param.name = "accel_lim_x";
+    double_param.value = 0.2;
+    conf.doubles.push_back(double_param);
+
+    double_param.name = "sim_time";
+    double_param.value = 5.0;
     conf.doubles.push_back(double_param);
 
     srv_req.config = conf;
@@ -29,12 +41,13 @@ void DynamicReconfigureSafeAction::execute(){
 };
 
 void DynamicReconfigureSafeAction::stop(){
+    ROS_INFO("Stopping setting to default params");
     dynamic_reconfigure::ReconfigureRequest srv_req;
     dynamic_reconfigure::ReconfigureResponse srv_resp;
     dynamic_reconfigure::BoolParameter bool_param;
     dynamic_reconfigure::Config conf;
-    
-    bool_param.name = "restore_default";
+
+    bool_param.name = "restore_defaults";
     bool_param.value = true;
     conf.bools.push_back(bool_param);
 
