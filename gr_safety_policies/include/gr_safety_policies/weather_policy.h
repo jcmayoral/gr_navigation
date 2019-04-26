1bdb2db918b870400638c33ee6e15ca2c6e1a286
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-04-26 18:42:29 +0200

    gr_safety_policies 0.3.2

--- gr_safety_policies/include/gr_safety_policies/weather_policy.h
+++ gr_safety_policies/include/gr_safety_policies/weather_policy.h
@@ -4,9 +4,20 @@
 #include <ros/ros.h>
 #include <string>
 #include <fault_core/fault_detector.h>
+#include <curl/curl.h>
+#include <json/json.h>
+
+//Based on 
+//https://gist.github.com/connormanning/41efa6075515019e499c
 
 namespace gr_safety_policies
 {
+  std::size_t callback(const char* in, std::size_t size,
+                       std::size_t num, std::string* out){
+      const std::size_t totalBytes(size * num);
+      out->append(in, totalBytes);
+      return totalBytes;
+  }
 
   class WeatherPolicy : public fault_core::FaultDetector
   {
--- gr_safety_policies/src/policies/weather_policy.cpp
+++ gr_safety_policies/src/policies/weather_policy.cpp
@@ -1,6 +1,5 @@
 #include "gr_safety_policies/weather_policy.h"
 #include <pluginlib/class_list_macros.h>
-#include <curl/curl.h>
 
 // register this class as a Fault Detector
 PLUGINLIB_DECLARE_CLASS(gr_safety_policies, WeatherPolicy,
@@ -14,9 +13,18 @@ namespace gr_safety_policies
             CURL *curl;
             CURLcode res;
             curl = curl_easy_init();
+            // Response information.
+            int httpCode(0);
+            std::unique_ptr<std::string> httpData(new std::string());
+            std::string url("https://www.yr.no/place/Mexico/Distrito_Federal/Mexico_City/forecast.xml");
+
             if(curl) {
-                  curl_easy_setopt(curl, CURLOPT_URL, "https://www.yr.no/place/Mexico/Distrito_Federal/Mexico_City/forecast.xml");
-                  /* example.com is redirected, so we tell libcurl to follow redirection */ 
+                  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+                  //just ipv4
+                  curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+                  //timeout n seconds
+                  curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+                  //redirect if necessary
                   curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
                   /* Perform the request, res will get the return code */ 
 
@@ -25,10 +33,54 @@ namespace gr_safety_policies
                   if(res != CURLE_OK)
                         fprintf(stderr, "curl_easy_perform() failed: %s\n",
                               curl_easy_strerror(res));
- 
-            /* always cleanup */ 
-            curl_easy_cleanup(curl);
+
+                  // Hook up data handling function.
+                  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+                  // Hook up data container (will be passed as the last parameter to the
+                  // callback handling function).  Can be any pointer type, since it will
+                  // internally be passed as a void pointer.
+                  curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+
+                  // Run our HTTP GET command, capture the HTTP response code, and clean up.
+                  curl_easy_perform(curl);
+                  curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+                  /* always cleanup */ 
+                  curl_easy_cleanup(curl);
             }
+            else{
+                  ROS_ERROR("Error");
+            }
+
+            if (httpCode == 200){
+                  std::cout << "\nGot successful response from " << url << std::endl;
+                  // Response looks good - done using Curl now.  Try to parse the results
+                  // and print them out.
+                  Json::Value jsonData;
+                  Json::Reader jsonReader;
+
+                  if (jsonReader.parse(*httpData.get(), jsonData)){
+                        std::cout << "Successfully parsed JSON data" << std::endl;
+                        std::cout << "\nJSON data received:" << std::endl;
+                        std::cout << jsonData.toStyledString() << std::endl;
+                        const std::string dateString(jsonData["date"].asString());
+                        const std::size_t unixTimeMs(jsonData["milliseconds_since_epoch"].asUInt64());
+                        const std::string timeString(jsonData["time"].asString());
+                        std::cout << "Natively parsed:" << std::endl;
+                        std::cout << "\tDate string: " << dateString << std::endl;
+                        std::cout << "\tUnix timeMs: " << unixTimeMs << std::endl;
+                        std::cout << "\tTime string: " << timeString << std::endl;
+                        std::cout << std::endl;
+                  }
+                  else{
+                        std::cout << "Could not parse HTTP data as JSON" << std::endl;
+                        std::cout << "HTTP data was:\n" << *httpData.get() << std::endl;
+                  }
+            }
+            else
+{
+                  std::cout << "Couldn't GET from " << url << " - exiting" << std::endl;
+            }
+
       }
 
       WeatherPolicy::~WeatherPolicy(){
