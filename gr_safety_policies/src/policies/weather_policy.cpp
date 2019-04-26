4e9b48fd6989f7d408fbe4edad7660a6a0f6a49f
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-04-26 16:32:19 +0200

    gr_safety_policies curl first version for weather policy

--- gr_safety_policies/src/policies/weather_policy.cpp
+++ gr_safety_policies/src/policies/weather_policy.cpp
@@ -1,5 +1,6 @@
 #include "gr_safety_policies/weather_policy.h"
 #include <pluginlib/class_list_macros.h>
+#include <curl/curl.h>
 
 // register this class as a Fault Detector
 PLUGINLIB_DECLARE_CLASS(gr_safety_policies, WeatherPolicy,
@@ -10,6 +11,24 @@ using namespace fault_core;
 namespace gr_safety_policies
 {
       WeatherPolicy::WeatherPolicy(){
+            CURL *curl;
+            CURLcode res;
+            curl = curl_easy_init();
+            if(curl) {
+                  curl_easy_setopt(curl, CURLOPT_URL, "https://www.yr.no/place/Mexico/Distrito_Federal/Mexico_City/forecast.xml");
+                  /* example.com is redirected, so we tell libcurl to follow redirection */ 
+                  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+                  /* Perform the request, res will get the return code */ 
+
+                  res = curl_easy_perform(curl);
+                  /* Check for errors */ 
+                  if(res != CURLE_OK)
+                        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                              curl_easy_strerror(res));
+ 
+            /* always cleanup */ 
+            curl_easy_cleanup(curl);
+            }
       }
 
       WeatherPolicy::~WeatherPolicy(){
