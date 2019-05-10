648f2484c256ed7aff9963d21d23d480f4b34221
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-05-10 14:19:01 +0200

    gr_safety_policies 0.4.4 action_manager_node improvements

--- gr_safety_policies/src/action_manager_node.cpp
+++ gr_safety_policies/src/action_manager_node.cpp
@@ -1,41 +1,52 @@
 #include <pluginlib/class_loader.h>
 #include <safety_core/safe_action.h>
+#include <memory>
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "action_manager_node");
 
+  int action_selected = -1;
+  std::map<int, std::string> action_map_;
+  boost::shared_ptr<safety_core::SafeAction> action;
+  std::string desired_str;
+  pluginlib::ClassLoader<safety_core::SafeAction> action_loader("safety_core", "safety_core::SafeAction");
+
   try {
-    pluginlib::ClassLoader<safety_core::SafeAction> action_loader("safety_core", "safety_core::SafeAction");
-    //check if a non fully qualified name has potentially been passed in
-    if(true){//}!action_loader.isClassAvailable(fault_detector_)){
-      std::vector<std::string> classes = action_loader.getDeclaredClasses();
-      for(unsigned int i = 0; i < classes.size(); ++i){
-        std::cout << classes[i] << std::endl;
+    std::cout << "Select Action to activate (-1 to abort) "<< std::endl;
 
-
-        /*
-        if(desired_id == fd_loader_.getName(classes[i])){
-        //if we've found a match... we'll get the fully qualified name and break out of the loop
-        ROS_WARN("Fault Detector specifications should now include the package name. You are using a deprecated API. Please switch from %s to %s in your yaml file.",
-            fault_detector_.c_str(), classes[i].c_str());
-        fault_detector_ = classes[i];
-        break;
-        */
-      }
+    std::vector<std::string> classes = action_loader.getDeclaredClasses();
+    for(unsigned int i = 0; i < classes.size(); ++i){
+      std::cout << i << ":" << classes[i] << std::endl;
+      action_map_[i] = classes[i];
     }
-  //}
 
-  /*
-  fd_ = fd_loader_.createInstance(fault_detector_);
-  ROS_INFO("Created fault_detector_ %s", fault_detector_.c_str());
-  //fd_->initialize(fd_loader_.getName(fault_detector_));
-  fd_->instantiateServices(nh);
-  */
-} catch (const pluginlib::PluginlibException& ex)
-{
-  ROS_FATAL("Failed to create the %s detector", ex.what());
-  exit(1);
-}
+    std::cin >> action_selected;
+
+    if (action_selected == -1){
+      ROS_WARN("ABORTING");
+      exit(0);
+    }
+
+    desired_str = classes[action_selected];
+
+    std::cout << "BASE CLASS TYPE " << action_loader.getBaseClassType() << std::endl;
+    std::cout << "CLASS TYPE " << action_loader.getClassType(desired_str) << std::endl;
+    std::cout << "CLASS Package " << action_loader.getClassPackage(desired_str) << std::endl;
+    std::cout << "name " << action_loader.getName(desired_str) << std::endl;
+
+    if(action_loader.isClassAvailable(desired_str)){
+      ROS_INFO("Available Action... Loading");
+      if(!action_loader.isLibraryAvailable(desired_str)){
+        ROS_WARN("NOT Available Action Library");
+      }
+      action = action_loader.createInstance(desired_str.c_str());
+      ROS_INFO("Created safe_action %s", classes[action_selected].c_str());
+    }
+  }
+  catch (const pluginlib::PluginlibException& ex){
+    ROS_FATAL("Failed to create action %s", ex.what());
+    exit(1);
+  }
 
 }
