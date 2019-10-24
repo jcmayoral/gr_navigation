a7bca72fefa0f68dbf4a5a7b9bc6f3549811bb7d
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-10-24 13:05:35 +0200

    removing duplicated files

--- gr_ml/setup.py
+++ gr_ml/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 d = generate_distutils_setup(
-    packages=['gr_ml'],
+    packages=['gr_ml', 'gr_terrain_recognition'],
     package_dir={'': 'src'}
 )
 
