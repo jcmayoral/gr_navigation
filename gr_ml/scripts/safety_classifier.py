fcac2eebceba932d6f0dff3766330c4390d87e4e
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-10-24 13:10:55 +0200

    gr_ml 0.0.10

--- gr_ml/scripts/safety_classifier.py
+++ gr_ml/scripts/safety_classifier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import rospy
-from gr_ml.pytorch_ros_interface import PyTorchWrapper
+from gr_ml.gr_safety.pytorch_ros_interface import PyTorchWrapper
 
 if __name__ == '__main__':
     rospy.init_node("safety_ml_node")
--- gr_ml/setup.py
+++ gr_ml/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 d = generate_distutils_setup(
-    packages=['gr_ml', 'gr_terrain_recognition'],
+    packages=['gr_ml'],
     package_dir={'': 'src'}
 )
 
