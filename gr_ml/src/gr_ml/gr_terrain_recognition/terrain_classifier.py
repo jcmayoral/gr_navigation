8afa3b591e32a9e064b5c67287a25b576c4cb74a
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-12-13 15:41:14 +0100

    spliting to gr_perception

--- gr_ml/src/gr_ml/gr_terrain_recognition/terrain_classifier.py
+++
@@ -1,6 +0,0 @@
-from gr_ml.gr_terrain_recognition.coder import   Features2Image
-import rospy
-
-class TerrainClassifier(Features2Image):
-    def __init__(self):
-        Features2Image.__init__(self,ros=True, pix_per_meter=1, msg_selection=3,meters=20, memory=25)#meters=10,pix_per_meter=1, msg_selection=2)
--- gr_tools/pcl_cuda_tools/include/pcl_cuda_tools/cuda_filter_functions.cuh
+++
@@ -1,9 +0,0 @@
-#ifndef FILTER_TOOLS_CUH
-#define FILTER_TOOLS_CUH
-
-extern "C"
-{
-  int apply_cuda_filter(float *o_z, bool *o_b, float min_limit, float max_limit, float filter_value, int size);
-}
-
-#endif
