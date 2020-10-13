06074ab3dc162d77b977391394ac7a98c24bc19e
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-10-13 13:29:20 +0200

    fixing wrong simulation with linear y and angular z velocity

--- gr_navigation_actions/simple_row_nav/simple_row_nav.py
+++ gr_navigation_actions/simple_row_nav/simple_row_nav.py
@@ -15,6 +15,7 @@ import sys
 import os
 import math
 import time
+import numpy as np
 
 import actionlib
 from gr_action_msgs.msg import SingleRowExecutionAction, SingleRowExecutionFeedback, SingleRowExecutionResult
@@ -33,7 +34,7 @@ class SimpleRowNavController:
         self.desired_speed = desired_speed
         self.is_next_required = False
         self.distance = 10.0
-        self.repetitions = 20
+        self.repetitions = 2
         self.listener = tf.TransformListener()
         self.initialize_test()
         self.init_bag = init_bag
@@ -68,7 +69,7 @@ class SimpleRowNavController:
 
         self.startpose = [0,0]
         self.endpose = [0,0]
-        self.currentpose = [0,0]
+        self.currentpose = [0,0,0]
         self.start = False
         self.forward = True
         self.command = None
@@ -96,6 +97,7 @@ class SimpleRowNavController:
             self._as.set_preempted()
             #self.emergency_stop()
         self.repetitions =  goal.repetitions
+        self.current_motions = 0
         self.is_not_aborted = True
         rospy.logerr( " Repeat %s times" % str(self.repetitions))
         folder_name = "/home/jose/ros_ws/src/gr_navigation/gr_navigation_actions/simple_row_nav/"+ goal.row_id.data +"/"
@@ -112,6 +114,9 @@ class SimpleRowNavController:
         self.twist.linear.x = goal.linearspeed
         self.desired_speed = goal.linearspeed
 
+        self.max_execution_time = 2*self.repetitions*(self.distance/goal.linearspeed)
+        rospy.logerr("MAX Execution Time " + str(self.max_execution_time))
+
         self.start_time = time.time()
 
         if self.init_bag:
@@ -123,14 +128,13 @@ class SimpleRowNavController:
         while self.is_running():
             rospy.sleep(0.1)
 
-
         self.command = None
 
         srvmessage = GetMetricsRequest()
         srvmessage.file_name = "srvtest.txt"
         resp = self._asclient(srvmessage)
         self.ac_result.metrics = resp.metrics
-        print ("Transcurred time " + str(time.time() - self.start_time) )
+        print ("Transcurred time " + str(time.time() - self.start_time) + str(self.check_time()) )
         if self.is_not_aborted:
             self.ac_result.message.data = "Success"
         else:
@@ -183,15 +187,35 @@ class SimpleRowNavController:
     def odom_cb(self, msg):
         self.currentpose[0] = msg.pose.pose.position.x
         self.currentpose[1] = msg.pose.pose.position.y
+
+        quaternion = (msg.pose.pose.orientation.x,
+                        msg.pose.pose.orientation.y,
+                        msg.pose.pose.orientation.z,
+                        msg.pose.pose.orientation.w)
+        self.currentpose[2] = tf.transformations.euler_from_quaternion(quaternion)[2]
         self.start = True
 
     def is_running(self):
-        return self.current_motions < self.repetitions and self.check_time()
+        flag = self.current_motions < self.repetitions and self.check_time()
+        if not flag:
+            self.twist.linear.x = 0
+            self.twist.linear.y = 0
+            self.twist.angular.z = 0
+        else:
+            #Line equation A = 1 B = 0 C = 0
+            n = 1*self.currentpose[0] + 0* self.currentpose[1] + 0
+            d = np.sqrt(np.power(1,2)+ np.power(0,2))
+            print n/d
+
+            self.twist.linear.y = -self.currentpose[1]#n/d
+            self.twist.angular.z = -self.currentpose[2]
+        return flag
+
 
     def check_time(self):
         current_time = time.time()
         self.is_not_aborted = (current_time - self.start_time) < self.max_execution_time
-        rospy.loginfo_throttle(5, "Transcurred time " + str(current_time - self.start_time) + "MAX" + str(self.max_execution_time))
+        #rospy.loginfo_throttle(5, "Transcurred time " + str(current_time - self.start_time) + "MAX" + str(self.max_execution_time))
         return self.is_not_aborted
 
     def publish(self, event):
@@ -205,9 +229,9 @@ class SimpleRowNavController:
         if self.is_next_required:
             self.change_direction()
         else:
-            rospy.loginfo_throttle(5,math.sqrt(math.pow(self.endpose[0] - self.currentpose[0],2) + math.pow(self.endpose[1] - self.currentpose[1],2)))
-            rospy.loginfo_throttle(5,max(3.0, self.desired_speed*3))
-            if math.sqrt(math.pow(self.endpose[0] - self.currentpose[0],2) + math.pow(self.endpose[1] - self.currentpose[1],2)) < max(3.0,self.desired_speed*3):
+            #rospy.loginfo_throttle(5,math.sqrt(math.pow(self.endpose[0] - self.currentpose[0],2) + math.pow(self.endpose[1] - self.currentpose[1],2)))
+            #rospy.loginfo_throttle(5,max(3.0, self.desired_speed*3))
+            if math.sqrt(math.pow(self.endpose[0] - self.currentpose[0],2) + math.pow(self.endpose[1] - self.currentpose[1],2)) < max(2.0,self.desired_speed*3):
                 self.change_direction()
 
 
@@ -219,7 +243,7 @@ class SimpleRowNavController:
         if self.forward:
             self.twist.linear.x = self.desired_speed
         else:
-            self.twist.linear.x = -0.25
+            self.twist.linear.x = -1.2
 
         self.current_motions = self.current_motions + 1
 
@@ -235,7 +259,7 @@ class SimpleRowNavController:
             self.swapPoses()
 
         self.is_next_required = False
-        self.start_time = time.time()
+        #self.start_time = time.time()
 
         if self.forward and self.is_running() and self.init_bag:
             self.rb.startBag()
