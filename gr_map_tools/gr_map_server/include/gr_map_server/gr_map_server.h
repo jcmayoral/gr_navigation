888a016c94f21bc05862462a9ea66ba5728e49f8
jcmayoral <jocamaba1989@gmail.com>
2021-05-13 18:50:17 +0200

    action server => probably will chaneg to mongo

--- gr_map_tools/gr_map_server/include/gr_map_server/gr_map_server.h
+++ gr_map_tools/gr_map_server/include/gr_map_server/gr_map_server.h
@@ -1,4 +1,8 @@
 #include <ros/ros.h>
+#include <gr_action_msgs/GREdgesAction.h>
+#include <actionlib/server/simple_action_server.h>
+
+using namespace gr_action_msgs;
 
 namespace gr_map_server{
     class GRMapServer{
@@ -6,5 +10,11 @@ namespace gr_map_server{
             GRMapServer();
             ~GRMapServer(){
             };
+        protected:
+            void execute_cb(const GREdgesActionGoal& goal);
+        private:
+            ros::NodeHandle nh_;
+            boost::shared_ptr<actionlib::SimpleActionServer<GREdgesAction>> server_;
+            ros::Topic
     };
 }
--- gr_map_tools/gr_map_server/src/gr_map_server.cpp
+++ gr_map_tools/gr_map_server/src/gr_map_server.cpp
@@ -2,6 +2,12 @@
 
 using namespace gr_map_server;
 
-GRMapServer::GRMapServer(){
+GRMapServer::GRMapServer(): nh_(""){
+    server_ = new actionlib::SimpleActionServer<GREdgesAction>(nh, "/topological/edges", boost::bind(&GRMapServer::execute_cb, _1, this), false);
+    server_->start();
+    ros::spin();
+}
+
+void GRMapServer::execute_cb(const GREdgesActionGoal& goal){
 
 }
