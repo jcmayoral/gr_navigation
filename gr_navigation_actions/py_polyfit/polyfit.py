21a44860f7c1ac7340e667dc806308adafceb89e
jcmayoral <jocamaba1989@gmail.com>
2021-05-18 20:27:05 +0200

    OK?

--- gr_navigation_actions/py_polyfit/polyfit.py
+++ gr_navigation_actions/py_polyfit/polyfit.py
@@ -5,37 +5,38 @@ import matplotlib.pyplot as plt
 class Action:
     def __init__(self):
         self.x = np.array([0,2,4])
-        self.y = np.array([0,0,0])
-        self.final = [4,0]
+        self.y = np.array([0,6,0])
+        self.final = [self.x[-1],self.y[-1]]
         self.nmotion = 8
+        self.trajectory = []
 
     def cost2goal(self, p):
-        return 2* np.sqrt(np.power(self.final[0] - p[0],2)+np.power(self.final[1] - p[1],2))
+        return np.sqrt(np.power(self.final[0] - p[0],2)+np.power(self.final[1] - p[1],2))
 
     def setStart(self):
-        self.startx = 0
-        self.starty = 0
+        self.startx = self.x[0]
+        self.starty = self.y[0]
 
-    def predict(self, motion):
+    def predict(self, motion,x,y):
         if motion == 0:
-            return self.startx + 0.1, self.starty
+            return x + 0.1, y
         if motion == 1:
-            return self.startx, self.starty + 0.1
+            return x, y + 0.1
         if motion == 2:
-            return self.startx + 0.1, self.starty + 0.1
+            return x + 0.1, y + 0.1
         if motion == 3:
-            return self.startx+0.1, self.starty-0.1
+            return x+0.1, y-0.1
         if motion == 4:
-            return self.startx - 0.1, self.starty+0.1
+            return x - 0.1, y+0.1
         if motion == 5:
-            return self.startx-0.1, self.starty-0.1
+            return x-0.1, y-0.1
         if motion == 6:
-            return self.startx-0.1, self.starty
+            return x-0.1, y
         if motion == 7:
-            return self.startx, self.starty-0.1
+            return x, y-0.1
 
     def apply(self, motion):
-        print ("Applying", motion)
+        print ("Applying", motion, self.startx, self.starty)
         if motion == 0:
             self.startx += 0.1
         if motion == 1:
@@ -56,26 +57,37 @@ class Action:
             self.startx -=0.1
         if motion == 7:
             self.starty -=0.1
+        print ("End", motion, self.startx, self.starty)
 
-    def evalMotion(self,x,y):
-        plt.scatter(x, y, c='b', s=50.0)
-        return np.fabs(y - np.polyval(self.coeff, x)) + self.cost2goal([x, y])
+
+    def evalMotion(self,x,y, t):
+        #plt.scatter(x, y, c='b', s=20.0*t)
+        return 0.2*np.fabs(y - np.polyval(self.coeff, x)) + 100.5*self.cost2goal([x, y])
 
     def evalStep(self):
-        plt.scatter(self.startx, self.starty, c='r', s=150.0)
-        return np.fabs(self.starty - np.polyval(self.coeff, self.startx)) + self.cost2goal([self.startx, self.starty])
+        #plt.scatter(self.startx, self.starty, c='r', s=150.0)
+        self.trajectory.append([self.startx, self.starty])
+        return 0.2*np.fabs(self.starty - np.polyval(self.coeff, self.startx)) + 0.5*self.cost2goal([self.startx, self.starty])
+
+    def plotTrajectory(self):
+        print (np.array(self.trajectory)[:,0].shape)
+        plt.plot(np.array(self.trajectory)[:,0], np.array(self.trajectory)[:,1], c='g')
 
     def run(self):
-        scores = []
+        scores = np.zeros(self.nmotion)
+        x = self.startx
+        y = self.starty
+
         for i in range(self.nmotion):
-            x,y = self.predict(i)
-            scores.append(self.evalMotion(x,y))
+            xs,ys = self.predict(i,x,y)
+            scores[i] += self.evalMotion(xs,ys,10)
+
         action_indx = np.argmin(scores/np.sum(scores))
         print (scores/np.sum(scores), action_indx)
         self.apply(action_indx)
         print self.evalStep()
 
-    def drawArrow(self,A, B):
+    def drawArr150ow(self,A, B):
         plt.arrow(A[0], A[1], B[0] - A[0], B[1] - A[1],
               head_width=1.0, length_includes_head=False)
 
@@ -101,7 +113,8 @@ class Action:
 a = Action()
 a.calculate()
 a.setStart()
-for i in range(200):
+for i in range(50):
     a.run()
     print ("STEP SCORE ", a.evalStep())
+a.plotTrajectory()
 plt.show()
