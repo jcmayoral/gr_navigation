f8816657c6589ece9d0b4342ea3a0bb53136065f
jcmayoral <jocamaba1989@gmail.com>
2021-05-19 19:39:03 +0200

    super hack row following

--- gr_navigation_actions/py_polyfit/polyfit.py
+++ gr_navigation_actions/py_polyfit/polyfit.py
@@ -4,61 +4,77 @@ import actionlib
 import rospy
 from gr_action_msgs.msg import PolyFitRowAction, PolyFitRowActionGoal
 import time
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
 
 class Action(object):
     def __init__(self):
         rospy.init_node("polyfit_server")
+        self.gpath_pub = rospy.Publisher("global", Path)
+        self.lpath_pub = rospy.Publisher("local", Path)
+
         self._as = actionlib.SimpleActionServer('polyfit_action', PolyFitRowAction, execute_cb=self.myexecution_cb, auto_start = False)
-        self.dt = 1.0
+        self.dt = 0.1
         self.x = np.array([0,2,4])
         self.y = np.array([0,0,0])
         self.thetas = np.array([0,0,0])
         self.final = [self.x[-1],self.y[-1], 0]
-        self.nmotion = 9
-        self.ac = 0.1
+        self.nmotion = 2
+        self.ac = 0.25
         self.max_vel = 1.0
         self.v = 0.0
         self.s = 0.0
         self.max_steer = np.pi/6
         self.trajectory = []
         self.motions = []
+        self.vels =[]
         self._as.start()
         print "server started"
 
     def myexecution_cb(self, goal):
         print "received"
         #plt.clf()
+        self.motions = []
+        self.vels =[]
         self.x = []
         self.y = []
         self.thetas = []
         self.trajectory = []
+        self.v = 0
 
         for xi,yi,zi in zip(goal.x, goal.y,goal.yaw):
             self.x.append(xi)
             self.y.append(yi)
             self.thetas.append(zi)
-        print (self.x)
-        #print (self.y)
-        #print (self.thetas)
+
+        if len(self.x) < 4:
+            rospy.loginfo("avoiding")
+            time.sleep(5)
+            self._as.set_succeeded()
+            return
 
         self.calculate()
         self.setStart()
 
-        for i in range(10):
+        for i in range(500):
             print "run ", i
             self.run()
             if self.complete():
                 break
         self.plotTrajectory()
+
+        print "MOTIONS ", self.motions
+        print "vels ", self.vels
+
+        time.sleep(3)
         self._as.set_succeeded()
 
     def cost2goal(self, p):
         return np.sqrt(np.power(self.final[0] - p[0],2)+np.power(self.final[1] - p[1],2))
 
     def complete(self):
-        print self.final, self.startx, self.starty
-        print "dist2goal", np.sqrt(np.power(self.final[0] - self.startx,2)+np.power(self.final[1] - self.starty,2))
-        return np.sqrt(np.power(self.final[0] - self.startx,2)+np.power(self.final[1] - self.starty,2)) < 0.15 and np.fabs(self.starttheta - self.final[2]) < 0.2
+        #print self.final, self.startx, self.starty
+        return np.sqrt(np.power(self.final[0] - self.startx,2)+np.power(self.final[1] - self.starty,2)) < 0.15 # and np.fabs(self.starttheta - self.final[2]) < 0.2
 
     def setStart(self):
         self.startx = self.x[0]
@@ -70,22 +86,41 @@ class Action(object):
     def predict(self, motion,x,y):
         L = 1.0
         psi = self.starttheta
+
+        time2stop = np.fabs(self.v*self.ac+0.1)
+        #print "TIME2 stop", np.fabs(self.v/time2stop)
+        if self.v > 0.0000:
+            self.stop = self.v/time2stop
+        else:
+            self.stop = 0.0
+
+        if self.cost2goal([x, y]) <= np.fabs(self.v/time2stop):
+            v1 = self.v - (self.ac * self.dt)
+            v1 = max(0.0, v1)
+            psi1 = psi+v1/L * self.s * self.dt
+            return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi)* self.dt, psi1, v1, self.s
+
+
         #v+ s=
         if motion == 0:
             v1 = self.v + (self.ac * self.dt)
             v1 = min(self.max_vel, v1)
-            psi1 = self.s+psi+v1/L * np.tan(self.s)
-            return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi1)* self.dt, psi, v1, self.s
+            #BRUTE FORCE STOP
+            time2stop = np.fabs(self.v*self.ac+0.1)
+            psi1 = psi+v1/L * self.s * self.dt
+            return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi)* self.dt, psi1, v1, self.s
 
         #v- s=
         if motion == 1:
             v1 = self.v - (self.ac * self.dt)
-            v1 = max(-self.max_vel, v1)
-            psi1 = self.s+psi+v1/L * np.tan(self.s)
-            return x + v1*np.cos(psi1)*self.dt, y + v1*np.sin(psi1)* self.dt, psi1, v1, self.s
+            v1 = max(0.0, v1)
+            #psi1 = self.s+psi+v1/L * np.tan(self.s)
+            psi1 = psi+v1/L * self.s * self.dt
+            return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi)* self.dt, psi1, v1, self.s
         #v= s=
         if motion == 2:
-            psi1 = self.s+psi+self.v/L * np.tan(self.s)
+            #psi1 = self.s+psi+self.v/L * np.tan(self.s)
+            psi1 = psi+self.v/L * self.s * self.dt
             return x + self.v*np.cos(psi1)*self.dt, y + self.v*np.sin(psi1)* self.dt,psi1, self.v, self.s
 
         #v+ s+
@@ -94,22 +129,22 @@ class Action(object):
             v1 = min(self.max_vel, v1)
             s1 = self.s +  0.1
             s1 = min(s1,self.max_steer)
-            psi1 = psi + s1 + v1/L * self.dt * 0.1
-            return x + v1*np.cos(psi1)*self.dt, y + v1*np.sin(psi1)* self.dt,psi1, v1, s1
+            psi1 = psi + v1/L * self.dt *s1
+            return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi)* self.dt,psi1, v1, s1
         #v- s+
         if motion == 4:
             v1 = self.v - (self.ac * self.dt)
             v1 = min(self.max_vel, v1)
             s1 = self.s+0.1
             s1 = min(s1,self.max_steer)
-            psi1 = psi + s1 + v1/L * self.dt * 0.1
-            return x + v1*np.cos(psi1)*self.dt, y + v1*np.sin(psi1)* self.dt, psi1, v1, s1
+            psi1 = psi + s1 * v1/L * self.dt
+            return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi)* self.dt, psi1, v1, s1
 
         #v= s+
         if motion == 5:
             s1 = self.s + 0.1
             s1 = min(s1,self.max_steer)
-            psi1 = psi + s1 + self.v/L * self.dt * 0.1
+            psi1 = psi + s1* self.v/L * self.dt
             return x + self.v*np.cos(psi)*self.dt, y + self.v*np.sin(psi)* self.dt, psi1, self.v, s1
 
         #v+ s-
@@ -118,7 +153,7 @@ class Action(object):
             v1 = min(self.max_vel, v1)
             s1 = self.s - 0.1
             s1 = max(-self.max_steer,s1)
-            psi1 = psi + s1 + v1/L * self.dt * 0.1
+            psi1 = psi + v1/L * self.dt * s1
             return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi)* self.dt, psi1, v1,s1
 
         #v- s-
@@ -126,22 +161,26 @@ class Action(object):
             v1 = self.v - (self.ac * self.dt)
             v1 = min(self.max_vel, v1)
             s1 = self.s - 0.1
-            s1 = max(-self.max_steer,s1)
-            psi1 = psi + s1 + v1/L * self.dt * 0.1
+            s1 = max(0,s1)
+            psi1 = psi + v1/L * self.dt * s1
             return x + v1*np.cos(psi)*self.dt, y + v1*np.sin(psi)* self.dt, psi1, v1,s1
 
         #v= s-
         if motion == 8:
             s1 = self.s - 0.1
             s1 = max(-self.max_steer,s1)
-            psi1 = psi+ s1 + self.v/L * self.dt * 0.1
+            psi1 = psi+ self.v/L * self.dt * s1
             return x + self.v*np.cos(psi)*self.dt, y + self.v*np.sin(psi)* self.dt, psi1, self.v,s1
 
 
     def apply(self, motion,states):
         L = 1.0
-        print "apply motion ", motion
+        if self.cost2goal([self.startx, self.starty]) <= self.stop:
+            motion = 1
+            print "apply motion ", motion
         new_state = states[motion]
+        self.motions.append(motion)
+        self.vels.append(new_state[3])
         self.startx = new_state[0]
         self.starty = new_state[1]
         self.v = new_state[3]
@@ -153,8 +192,9 @@ class Action(object):
     def evalMotion(self,x,y, theta):
         #plt.scatter(x, y, c='b', s=20.0)
         #print "costs ", 20*np.fabs(y - np.polyval(self.coeff, x)) , 0.25*self.cost2goal([x, y]) , 20.0 *np.fabs(theta-self.final[2])
-        cost = 0.250*np.fabs(y - np.polyval(self.coeff, x)) + 100.0*self.cost2goal([x, y]) + 0.20 *np.fabs(theta-self.final[2]) + 0.5*(self.max_vel-self.v)
-        print ("cost ", cost)
+        cost = 5.0*self.cost2goal([x, y]) + 100.0 *int(self.stop > self.cost2goal([x, y]))# 0.5*(self.max_vel-self.v)
+
+        #cost = 0.50*np.fabs(x - np.polyval(self.coeff, y)) + 5.0*self.cost2goal([x, y]) + 0.20 *np.fabs(theta-self.final[2]) + 100.0 *int(self.stop > self.cost2goal([x, y]))# 0.5*(self.max_vel-self.v)
         return cost
 
     def evalStep(self):
@@ -163,7 +203,17 @@ class Action(object):
         return self.evalMotion(self.startx, self.starty, self.starttheta)
 
     def plotTrajectory(self):
-        print ("trajectory", self.trajectory)
+        p = Path()
+        p.header.frame_id = "map"
+        for xi,yi,zi in self.trajectory:
+            ps = PoseStamped()
+            ps.header.frame_id = "map"
+            ps.pose.position.x = xi
+            ps.pose.position.y = yi
+            ps.pose.orientation.w = 1.0
+            p.poses.append(ps)
+
+        self.lpath_pub.publish(p)
         plt.plot(np.array(self.trajectory)[:,0], np.array(self.trajectory)[:,1], c='g')
         plt.draw()
         #time.sleep(100)
@@ -183,7 +233,7 @@ class Action(object):
             states[i] = [xs,ys,ts,vs,ss]
             scores[i] += self.evalMotion(xs,ys,ts)
 
-        print ("states",states)
+        #print ("states",states)
 
         action_indx = np.argmin(scores/np.sum(scores))
         #print (scores/np.sum(scores), action_indx)
@@ -197,15 +247,28 @@ class Action(object):
     def calculate(self):
         s0 = self.thetas[0]#np.pi/2
         sf = self.thetas[-1]
-        self.coeff = (np.polyfit(self.x,self.y,2))
-        x1 = np.linspace(np.min(self.x),np.max(self.x),10)
-        y1 = np.polyval(self.coeff,x1)
+        self.coeff = (np.polyfit(self.y,self.x,3))
+        y1 = np.linspace(np.min(self.y),np.max(self.y),10)
+        x1 = np.polyval(self.coeff,y1)
         ang3 = []
         for i in range(len(x1)-1):
             ang3.append(np.arctan2(y1[i+1]-y1[i],x1[i+1]-x1[i]))
 
         ang1 = np.arctan2(y1,x1)
         ang2 = np.linspace(s0,sf, len(x1))
+
+        p = Path()
+        p.header.frame_id = "map"
+        for xi,yi in zip(x1,y1):
+            ps = PoseStamped()
+            ps.header.frame_id = "map"
+            ps.pose.position.x = xi
+            ps.pose.position.y = yi
+            ps.pose.orientation.w = 1.0
+            p.poses.append(ps)
+
+        self.gpath_pub.publish(p)
+
         #print ( ang1, "angulars")
         #plt.plot(x1,np.asarray(y1), c="b")
         #plt.scatter(self.x,self.y, c="b")
