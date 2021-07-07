9f7a16dfd1e0f30711fea1bdf0fdc8c681a6e55a
jcmayoral <jocamaba1989@gmail.com>
2021-07-07 15:22:22 +0200

    removing gr_map_server

--- gr_map_tools/gr_map_server/src/gr_map_server_node.cpp
+++
@@ -1,8 +0,0 @@
-#include <gr_map_server/gr_map_server.h>
-#include <ros/ros.h>
-using namespace gr_map_server;
-
-int main(int argc, char**argv){
-    ros::init(argc, argv,"gr_map_server");
-    GRMapServer server;
-}
