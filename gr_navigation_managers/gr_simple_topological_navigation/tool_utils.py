8c563feee31da321ccb36cd6a7f5d099f9b2fe20
Jose Mayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-09-05 22:42:32 +0200

    handling no-tool

--- gr_navigation_managers/gr_simple_topological_navigation/tool_utils.py
+++ gr_navigation_managers/gr_simple_topological_navigation/tool_utils.py
@@ -2,17 +2,24 @@ import rospy
 from std_msgs.msg import Int16, String|
 
 class ToolInterface:
-    def __init__(self, topic, type_str, high_value, low_value):
+    def __init__(self, topic=None, type=None, high_value=0, low_value=0):
         print ("TOPIC {} type{}".format(topic, type_str))
         self.high_value = high_value
         self.low_value = low_value
         self.type = self.create_type(type_str)
-        self.pub = rospy.Publisher(topic, self.type, queue_size = 1)
+        if topic is not None:
+            self.pub = rospy.Publisher(topic, self.type, queue_size = 1)
+        else:
+            self.pub = None
 
     def start(self):
+        if self.pub is None:
+            return
         self.pub.publish(self.type(data=self.high_value))
 
     def stop(self):
+        if self.pub is None:
+            return
         self.pub.publish(self.type(data=self.low_value))
 
     def create_type(self,type):
@@ -20,5 +27,7 @@ class ToolInterface:
             return Int16
         if type == "string":
             return String
+        if type is None:
+            return None
         else:
             raise ValueError("type " + type + "not recognized")
